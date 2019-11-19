5e6fde315752f91dd2ab29ec060fe14ddd75d6b9
xinyufei <xyfei27@gmail.com>
2019-11-18 20:44:28 -0500

    Add files via upload

---
+++ import_data.py
@@ -0,0 +1,91 @@
+import xdrlib ,sys
+import xlrd
+import numpy as np
+# specify the traffic network in plymouth road
+num_iterations = 1000
+T = 240
+N = 6 # number of intersetions
+ 
+ 
+file= 'Plymouth_Structure2.xlsx'
+data = xlrd.open_workbook(file)
+table = data.sheet_by_index(0)
+nrows = table.nrows
+ncols = table.ncols
+excel_matrix = np.zeros((nrows-1, ncols))
+print(nrows)
+print(ncols)
+for row in range(1, nrows):
+    for col in range(ncols):
+        cell_value = table.cell(row, col).value
+        excel_matrix[row - 1, col] = cell_value
+
+#type 1: ordinary
+#type 2: merge
+#type 3: diverge
+#type 4: intersection?
+#type 5: origin
+#type 6: destination?
+#type 7: dummy
+
+
+C_ALL = list(range(nrows - 1))
+O_ALL = []
+M_ALL = []
+V_ALL = []
+D_ALL = []
+DU_ALL = []
+I_ALL = [None]*N
+#I_ALL: num_intersection * num_phase * []
+for ind in range(N):
+    I_ALL[ind] = [None]*4
+    for jnd in range(4):
+        I_ALL[ind][jnd] = []
+
+pred_all = [None]*len(C_ALL)
+proc_all = [None]*len(C_ALL)
+jam = np.zeros(len(C_ALL))
+Q = np.zeros(len(C_ALL))
+beta = np.zeros((len(C_ALL),3))
+demand = np.zeros(len(C_ALL))
+
+for c in range(nrows-1):
+    type_c = excel_matrix[c,1]
+    if type_c == 2:
+        M_ALL.append(c)
+    elif type_c == 3:
+        V_ALL.append(c)
+    elif type_c == 4:
+        phase = int(excel_matrix[c,15]-1)
+        if phase == -1:
+            phase = int(excel_matrix[c-1,15]-1)
+        inter = int(excel_matrix[c, 17]-1)
+        I_ALL[inter][phase].append(c)
+    elif type_c == 5:
+        O_ALL.append(c)
+    elif type_c == 6:
+        D_ALL.append(c)
+    elif type_c == 7:
+        DU_ALL.append(c)
+
+    pred = []
+    for ind in range(int(excel_matrix[c,2])):
+        pred.append(excel_matrix[c,3+ind] - 1)
+    pred_all[c] = pred
+    
+    proc = []
+    for ind in range(int(excel_matrix[c,6])):
+        proc.append(excel_matrix[c,7+ind] - 1)
+    proc_all[c] = proc
+
+    jam[c] = excel_matrix[c,10]
+
+    Q[c] = excel_matrix[c,11]
+
+    beta[c,:] = excel_matrix[c, 12:15]
+
+    demand[c] = excel_matrix[c, 16]/1800
+
+
+alpha = -1.6
+W = 1/3
---
+++ lp_opt_fixw.py
@@ -0,0 +1,66 @@
+import numpy as np
+import gurobipy as gb
+from import_data import *
+
+I1_ALL = []
+I2_ALL = []
+I3_ALL = []
+I4_ALL = []
+for i in range(N):
+    for k in range(len(I_ALL[i][0])):
+        I1_ALL.append(I_ALL[i][0][k])
+    for k in range(len(I_ALL[i][1])):
+        I2_ALL.append(I_ALL[i][1][k])
+    for k in range(len(I_ALL[i][2])):
+        I3_ALL.append(I_ALL[i][2][k])
+    for k in range(len(I_ALL[i][3])):
+        I4_ALL.append(I_ALL[i][3][k])
+
+w = np.zeros((N,4,T))
+for t in range(15):
+    w[:,1,t*16:t*16+8]=1
+    w[:,1,t*16+8:(t+1)*16]=0
+
+m = gb.Model()
+y = m.addVars(len(C_ALL), T, lb=0, vtype=gb.GRB.CONTINUOUS)
+n = m.addVars(len(C_ALL), T+1, lb=0, vtype=gb.GRB.CONTINUOUS)
+m.setObjective(gb.quicksum(gb.quicksum(t * y[c,t] for c in D_ALL) for t in range(T))
+        + alpha * gb.quicksum(gb.quicksum(t* y[c,t] for c in list(set(C_ALL)-set(D_ALL))) for t in range(T)), gb.GRB.MINIMIZE)
+
+m.addConstrs(y[c,t]-n[c,t]<=0 for c in C_ALL for t in range(T))
+m.addConstrs(y[c,t]<=Q[c] for c in list(set(C_ALL)-set(I1_ALL)-set(I2_ALL)-set(I3_ALL)-set(I4_ALL)-set(D_ALL)) for t in range(T))
+m.addConstrs(y[c,t]-w[i,j,t]*Q[c]<=0 for i in range(N) for j in range(4) for c in I_ALL[i][j] for t in range(T))
+m.addConstrs(y[c,t]+W*n[proc_all[c][0],t]<=W*jam[c] for c in list(set(C_ALL)-set(D_ALL)-set(V_ALL)-set(DU_ALL)) for t in range(T))
+
+m.addConstrs(beta[c,0]*y[c,t]+W*n[c+1,t]<=W*jam[c+1] for c in V_ALL for t in range(T))
+m.addConstrs(beta[c,1]*y[c,t]+W*n[c+2,t]<=W*jam[c+2] for c in V_ALL for t in range(T))
+m.addConstrs(beta[c,2]*y[c,t]+W*n[c+3,t]<=W*jam[c+3] for c in V_ALL for t in range(T))
+m.addConstrs(n[c,t+1] - n[c,t] - beta[c,int(c-pred_all[c][0]-1)]*y[int(pred_all[c][0]),t] + y[c,t] == 0 for c in I1_ALL+I2_ALL+I3_ALL+I4_ALL for t in range(T))
+
+m.addConstrs(n[c,t+1] - n[c,t] - y[pred_all[c][0],t] + y[c,t] == 0 for c in list(set(C_ALL)-set(O_ALL)-set(I1_ALL)-set(I2_ALL)-set(I3_ALL)-set(I4_ALL)-set(M_ALL)) for t in range(T))
+m.addConstrs(n[c,t+1] - n[c,t] - gb.quicksum(y[d,t] for d in pred_all[c]) + y[c,t] == 0 for c in M_ALL for t in range(T))
+m.addConstrs(n[c,t+1] - n[c,t] + y[c,t] == demand[c] for c in O_ALL for t in range(T))
+#m.addConstrs(n[c,T]==0 for c in C_ALL)
+m.addConstrs(n[c,0] == 1 for c in C_ALL)
+
+m.Params.TimeLimit=7200
+m.Params.LogFile = "log/LP_fix_Gurobi_T240.log"
+m.Params.LogToConsole = 0
+m.optimize()
+opt = m.objval
+
+n_value = m.getAttr('X', n)
+result_n = np.zeros((len(C_ALL), T))
+for i in range(len(C_ALL)):
+    for t in range(T):
+        result_n[i, t] = n_value[i, t]
+
+f = open('log/n.log', 'w+')
+for i in range(len(C_ALL)):
+    for t in range(T):
+        print(result_n[i, t], end="   ", file=f)
+    print("\n", file=f)
+f.close()
+
+
+
